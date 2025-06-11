8c7aea17bf678d797fa443623ce5c24bccd5a525
eareimu <reimu@genmeta.net>
2025-06-11 20:10:58 +0800

    feat: release gmutils v0.2.7

--- gmutils.rb
+++ gmutils.rb
@@ -1,15 +1,15 @@
 class Gmutils < Formula
   desc "Genmeta Binary Utilities"
-  version "0.2.6"
+  version "0.2.7"
 
   on_arm do
-    url "https://download.genmeta.net/homebrew/gmutils_0.2.6_arm64.tar.gz"
-    sha256 "d0060cd4f89a9ec8ddcfd1fa8a23e621c87ab10b88d8fc699b0bb5d24e6fbce5"
+    url "https://download.genmeta.net/homebrew/gmutils_0.2.7_arm64.tar.gz"
+    sha256 "b92193642d44e139ef4fc81e1bd8d053616dcc4cd8bdbb21038d6dc248c04df7"
   end
   
   on_intel do
-    url "https://download.genmeta.net/homebrew/gmutils_0.2.6_amd64.tar.gz"
-    sha256 "6022104f17606ae710d3265af357c039e46ff2e8f21182aabde95168fffa957c"
+    url "https://download.genmeta.net/homebrew/gmutils_0.2.7_amd64.tar.gz"
+    sha256 "2206626b28375255d5a6a2c825efe21133a69d6e304aaeecd9a3af7dc7a0e489"
   end
 
   def install
