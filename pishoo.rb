4454642948b391a565a36d650fd93ae411400fbf
eareimu <reimu@genmeta.net>
2025-07-30 14:44:26 +0800

    feat: release pishoo v0.3.1

--- pishoo.rb
+++ pishoo.rb
@@ -1,15 +1,15 @@
 class Pishoo < Formula
   desc "Pishoo (\"Prosperity Guardian Beast\") is a powerful proxy server optimized for HTTP/3 and end-to-end encrypted communication. Built for privacy and security scenarios, it seamlessly functions as both a forward proxy for client privacy and a reverse proxy to streamline traffic between edge networks and backend services. Its architecture is designed to safeguard your data from infringement and ensures that it can be accessed and utilized securely."
-  version "0.3.0"
+  version "0.3.1"
 
   on_arm do
-    url "https://download.genmeta.net/homebrew/pishoo_0.3.0_arm64.tar.gz"
-    sha256 "9d36cae10d98cc2cdfb9c4ae39b8ae2d8c1e6d9856fa51c8ed7f000f60bf6ad4"
+    url "https://download.genmeta.net/homebrew/pishoo_0.3.1_arm64.tar.gz"
+    sha256 "47463b7b0ff297fc2c912907b488203648878086abf46d825f025e00ce71b42e"
   end
   
   on_intel do
-    url "https://download.genmeta.net/homebrew/pishoo_0.3.0_amd64.tar.gz"
-    sha256 "5e23789d43fd21bd5d4ce72f56a2557be77f5bc14a5366849f2e78c53271ca2e"
+    url "https://download.genmeta.net/homebrew/pishoo_0.3.1_amd64.tar.gz"
+    sha256 "c5d571e94a678ad88a96466562dcdeaa2e5c3e4195fcb2e6b8c102dd3461193b"
   end
 
   def install
