9141f99f1f8f11200de54851406e3f93b7016e39
eareimu <reimu@genmeta.net>
2026-04-21 17:44:27 +0800

    feat: update pishoo download domain to dhttp.net

--- pishoo.rb
+++ pishoo.rb
@@ -4,12 +4,12 @@ class Pishoo < Formula
   license "Shareware"
 
   on_arm do
-    url "https://download.genmeta.net/homebrew/pishoo_0.5.0-aarch64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/pishoo_0.5.0-aarch64-apple-darwin.tar.gz"
     sha256 "23a09012936610e2f19130ce13c79fb73e9e7c4f7c4ecb9e8d69b6792eb4e2cd"
   end
 
   on_intel do
-    url "https://download.genmeta.net/homebrew/pishoo_0.5.0-x86_64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/pishoo_0.5.0-x86_64-apple-darwin.tar.gz"
     sha256 "c873c4b5bf1e2a379e5fa2fb792eb348f027f374c0ff9cf87793e25f9e305d56"
   end
 
