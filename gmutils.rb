2a93ccf7c11c2e9b6b7e0ff6bfacc92fe1ed2f4a
eareimu <reimu@genmeta.net>
2026-04-21 17:44:27 +0800

    feat: update gmutils download domain to dhttp.net

--- gmutils.rb
+++ gmutils.rb
@@ -4,12 +4,12 @@ class Gmutils < Formula
   license "Shareware"
 
   on_arm do
-    url "https://download.genmeta.net/homebrew/gmutils-0.5.0-aarch64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/gmutils-0.5.0-aarch64-apple-darwin.tar.gz"
     sha256 "398dfe39935cc6fae5da6edaeedf0bba53d5550c68f65876b57727e6e0f26696"
   end
 
   on_intel do
-    url "https://download.genmeta.net/homebrew/gmutils-0.5.0-x86_64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/gmutils-0.5.0-x86_64-apple-darwin.tar.gz"
     sha256 "129744cb509c430336e1432892727c4795752fedb84fd13a8ffd3893a58fcc73"
   end
 
