960c432138aa70a0e1b9bf75c080d2da1d2c3fed
eareimu <reimu@genmeta.net>
2026-07-17 01:47:45 +0800

    fix: point gmutils formula at stable channel

--- gmutils.rb
+++ gmutils.rb
@@ -5,12 +5,12 @@ class Gmutils < Formula
   license "Apache-2.0"
 
   on_arm do
-    url "https://download.dhttp.net/homebrew/gmutils-0.7.0-aarch64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/stable/gmutils-0.7.0-aarch64-apple-darwin.tar.gz"
     sha256 "a85cf0d022ab7a748c8d0d4359a8ed9c6c4b134eae5bc3f9350a4cad85d1a03b"
   end
 
   on_intel do
-    url "https://download.dhttp.net/homebrew/gmutils-0.7.0-x86_64-apple-darwin.tar.gz"
+    url "https://download.dhttp.net/homebrew/stable/gmutils-0.7.0-x86_64-apple-darwin.tar.gz"
     sha256 "3f7a9ba797a9ee99faa033f8f128abcfa5e0696a49aeb04166f99e032f447f2e"
   end
 
